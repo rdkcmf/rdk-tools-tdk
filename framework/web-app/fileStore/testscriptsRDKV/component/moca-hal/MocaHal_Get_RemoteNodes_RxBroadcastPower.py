824841567540766b22cc3d74bd3eb6d03b177b1c
Swapna S M <swapna_mohanakumari@comcast.com>
2019-12-30 12:36:18 +0000

    RDKTT-1835 : TDK-V Coverage enhancement - MoCA HAL
    Reason for change: Added new scripts for moca-hal module
    Test Procedure: Run moca-hal scripts using TM
    Risks: None
    Signed-off-by: Swapna S M <swapna_mohanakumari@comcast.com>
    
    (cherry picked from commit e0bb08786f9ed39bea11f7ecc748694ce6625d87)
    
    Change-Id: I90e7193434d140895752523403543c98e5080e10

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/moca-hal/MocaHal_Get_RemoteNodes_RxBroadcastPower.py
@@ -0,0 +1,125 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>1</version>
+  <name>MocaHal_Get_RemoteNodes_RxBroadcastPower</name>
+  <primitive_test_id/>
+  <primitive_test_name>MocaHal_RemoteNode_GetRxBroadcastPower</primitive_test_name>
+  <primitive_test_version>1</primitive_test_version>
+  <status>FREE</status>
+  <synopsis>To get the RxBroadcastPower of the remote node</synopsis>
+  <groups_id/>
+  <execution_time>1</execution_time>
+  <long_duration>false</long_duration>
+  <advanced_script>false</advanced_script>
+  <remarks/>
+  <skip>false</skip>
+  <box_types>
+    <box_type>IPClient-3</box_type>
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>CT_MOCA_HAL_68</test_case_id>
+    <test_objective>To get the RxBroadcastPower of the remote node</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>XI3</test_setup>
+    <pre_requisite>1.Initialize the moca handle
+RMH_Initialize(NULL, NULL);
+2.Destroy the moca handle at the end of the test.</pre_requisite>
+    <api_or_interface_used>SoC_IMPL__RMH_RemoteNode_GetRxBroadcastPower</api_or_interface_used>
+    <input_parameters>[in]	handle	The RMH handle as preturned by RMH_Initialize.
+[in]    nodeId	The node Id of the remote node to inspect	
+[out]	response	The RxBroadcastPower of the remote node.</input_parameters>
+    <automation_approch>1. TM loads the Moca hal agent via the test agent.
+2. Moca hal agent will invoke the api SoC_IMPL__RMH_Network_GetRemoteNodeIds to get list of remote node ids
+3. Moca hal agent will invoke the api SoC_IMPL__RMH_RemoteNode_GetRxBroadcastPower with each node id
+4. Check the api call return value and return SUCCESS/FAILURE status.</automation_approch>
+    <except_output>Checkpoint 1.Verify the API call return value</except_output>
+    <priority>High</priority>
+    <test_stub_interface>libmocahalstub.so</test_stub_interface>
+    <test_script>MocaHal_Get_RemoteNodes_RxBroadcastPower</test_script>
+    <skipped>No</skipped>
+    <release_version>M72</release_version>
+    <remarks/>
+  </test_cases>
+</xml>
+
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+
+#Test component to be tested
+mocahalObj = tdklib.TDKScriptingLibrary("mocahal","2.0");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+mocahalObj.configureTestCase(ip,port,'MocaHal_Get_RemoteNodes_RxBroadcastPower');
+
+#Get the result of connection with test component and STB
+mocaLoadStatus =mocahalObj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %mocaLoadStatus;
+mocahalObj.setLoadModuleStatus(mocaLoadStatus.upper());
+
+if "SUCCESS" in mocaLoadStatus.upper():
+    expectedresult="SUCCESS"
+    #Prmitive test case to get remote Node Ids
+    tdkTestObj = mocahalObj.createTestStep('MocaHal_GetRemoteNodeIds');
+    #Execute the test case in STB
+    tdkTestObj.executeTestCase(expectedresult);
+    actualresult = tdkTestObj.getResult();
+    print "RESULT: Remote NodeIds : " , actualresult
+    remoteNodeList = tdkTestObj.getResultDetails().split(',');
+    print "DETAILS: Remote NodeIds : " , remoteNodeList
+
+    if expectedresult in actualresult and remoteNodeList:
+        tdkTestObj.setResultStatus("SUCCESS")
+        print "Remote NodeIds retrieved" , remoteNodeList
+
+        for nodeId in remoteNodeList:
+            #Prmitive test case to get remote node RxBroadcastPower
+            tdkTestObj = mocahalObj.createTestStep('MocaHal_RemoteNode_GetRxBroadcastPower');
+            tdkTestObj.addParameter("nodeId",int(nodeId));
+            #Execute the test case in STB
+            tdkTestObj.executeTestCase(expectedresult);
+            actualresult = tdkTestObj.getResult();
+            print "RESULT: RemoteNode_GetRxBroadcastPower : " , actualresult
+            power = tdkTestObj.getResultDetails();
+            print "DETAILS: RemoteNode_GetRxBroadcastPower : " , power
+
+            if expectedresult in actualresult and power:
+                tdkTestObj.setResultStatus("SUCCESS")
+                print "RxBroadcastPower %s retrieved for node id %d" %(power, int(nodeId))
+            else:
+                tdkTestObj.setResultStatus("FAILURE")
+                print "RxBroadcastPower not retrieved for node id ", int(nodeId)
+    else:
+        tdkTestObj.setResultStatus("FAILURE")
+        print "Remote NodeIds not retrieved"
+
+    mocahalObj.unloadModule("mocahal");
+
+else:
+    print "Failed to load moca hal module\n";
+
---
+++ framework/web-app/fileStore/testscriptsRDKV/component/moca-hal/MocaHal_Get_SelfNode_TxDroppedPackets.py
@@ -0,0 +1,148 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2019 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>1</version>
+  <name>MocaHal_Get_SelfNode_TxDroppedPackets</name>
+  <primitive_test_id/>
+  <primitive_test_name>MocaHal_GetTxDroppedPackets</primitive_test_name>
+  <primitive_test_version>1</primitive_test_version>
+  <status>FREE</status>
+  <synopsis>To get the total number of packets dropped during transmission</synopsis>
+  <groups_id/>
+  <execution_time>1</execution_time>
+  <long_duration>false</long_duration>
+  <advanced_script>false</advanced_script>
+  <remarks/>
+  <skip>false</skip>
+  <box_types>
+    <box_type>IPClient-3</box_type>
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>CT_MOCA_HAL_56</test_case_id>
+    <test_objective>To get the total number of packets dropped during transmission</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>XI3</test_setup>
+    <pre_requisite>1.Initialize the moca handle
+RMH_Initialize(NULL, NULL);
+2.Destroy the moca handle at the end of the test.</pre_requisite>
+    <api_or_interface_used>SoC_IMPL__RMH_Stats_GetTxDroppedPackets</api_or_interface_used>
+    <input_parameters>[in]	handle	The RMH handle as preturned by RMH_Initialize.
+[out]	response	The number of packets dropped</input_parameters>
+    <automation_approch>1. TM loads the Moca hal agent via the test agent.
+2. Using systemutil ExecuteCmd command get the tx dropped packets using linux command
+3. Moca hal agent will invoke the api SoC_IMPL__RMH_Stats_GetTxDroppedPackets 
+4. Check the value from mocahal interface is greater than or equal to the value from linux command and return SUCCESS/FAILURE status.</automation_approch>
+    <except_output>Checkpoint 1.Verify the API call return value
+                   Checkpoint 2.Verify the value from mocahal interface is greater than or equal to the value from linux command</except_output>
+    <priority>High</priority>
+    <test_stub_interface>libmocahalstub.so</test_stub_interface>
+    <test_script>MocaHal_Get_SelfNode_TxDroppedPackets</test_script>
+    <skipped>No</skipped>
+    <release_version>M72</release_version>
+    <remarks/>
+  </test_cases>
+</xml>
+
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+
+#Test component to be tested
+mocahalObj = tdklib.TDKScriptingLibrary("mocahal","2.0");
+sysObj = tdklib.TDKScriptingLibrary("systemutil","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+mocahalObj.configureTestCase(ip,port,'MocaHal_Get_SelfNode_TxDroppedPackets');
+sysObj.configureTestCase(ip,port,'MocaHal_Get_SelfNode_TxDroppedPackets');
+
+#Get the result of connection with test component and STB
+mocaLoadStatus = sysObj.getLoadModuleResult();
+print "[MOCA HAL LIB LOAD STATUS]  :  %s" %mocaLoadStatus;
+mocahalObj.setLoadModuleStatus(mocaLoadStatus.upper());
+sysLoadStatus = sysObj.getLoadModuleResult();
+print "[SYSTEM UTIL LIB LOAD STATUS]  :  %s" %sysLoadStatus;
+sysObj.setLoadModuleStatus(sysLoadStatus.upper());
+
+if "SUCCESS" in mocaLoadStatus.upper() and sysLoadStatus.upper():
+    expectedResult="SUCCESS"
+
+    #Prmitive test case
+    tdkTestObj = mocahalObj.createTestStep('MocaHal_GetName');
+    #Execute the test case in STB
+    tdkTestObj.executeTestCase(expectedResult);
+    actualResult = tdkTestObj.getResult();
+    print "RESULT: MocaHal_GetName : " , actualResult
+    ifName = tdkTestObj.getResultDetails();
+    print "DETAILS: MocaHal_GetName : " , ifName 
+
+    if expectedResult in actualResult and ifName:
+        tdkTestObj.setResultStatus("SUCCESS")
+        print "Self node Interface name retrieved: ", ifName
+        #Get TxDroppedPackets using system command
+        tdkTestObj = sysObj.createTestStep('ExecuteCommand');
+        cmd = "cat /sys/class/net/" + ifName + "/statistics/tx_dropped | tr -d '\n'";
+        tdkTestObj.addParameter("command", cmd);
+        tdkTestObj.executeTestCase("SUCCESS");
+        actualResult = tdkTestObj.getResult();
+        txPackets = tdkTestObj.getResultDetails();
+        print "TxDroppedPackets from linux command: ",txPackets
+
+        if expectedResult in actualResult and txPackets:
+            tdkTestObj.setResultStatus("SUCCESS")
+            #Prmitive test case 
+            tdkTestObj = mocahalObj.createTestStep('MocaHal_GetTxDroppedPackets');
+            #Execute the test case in STB
+            tdkTestObj.executeTestCase(expectedResult);
+            actualResult = tdkTestObj.getResult();
+            print "RESULT: GetTxDroppedPackets : " , actualResult
+            txDroppedPackets = tdkTestObj.getResultDetails();
+            print "DETAILS: GetTxDroppedPackets : " , txDroppedPackets
+
+            if expectedResult in actualResult and txDroppedPackets:
+                print "TxDroppedPackets retrieved: ", txDroppedPackets
+                if txDroppedPackets >= txPackets:
+                    tdkTestObj.setResultStatus("SUCCESS")
+                    print "TxDroppedPackets verified"
+                else:
+                    tdkTestObj.setResultStatus("FAILURE")
+                    print "TxDroppedPackets not verified"
+            else:
+                tdkTestObj.setResultStatus("FAILURE")
+                print "TxDroppedPackets not retrieved"
+        else:
+            tdkTestObj.setResultStatus("FAILURE")
+            print "Execute command failed to get tx dropped packets"
+    else:
+        tdkTestObj.setResultStatus("FAILURE")
+        print "Failed to get self node interface name"
+
+    mocahalObj.unloadModule("mocahal");
+    sysObj.unloadModule("systemutil");
+
+else:
+    print "Module loading FAILURE";
+
