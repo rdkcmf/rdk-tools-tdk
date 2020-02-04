d1056cda100772d7fc9d46c17cfd6414d56eeda6
Swapna S M <swapna_mohanakumari@comcast.com>
2020-02-04 04:23:51 +0000

    RDKTT-1856 : TDK-V Coverage enhancement - DS HAL
    Reason for change: Added stub and scripts for DS HAL module
    Test Procedure: Run DS HAL scripts using TM
    Risks: None
    Signed-off-by: Swapna S M <swapna_mohanakumari@comcast.com>
    
    Change-Id: Ifc0656ab6e49ad6d82c1ac98385f3283a94786f6

--- framework/web-app/fileStore/testscriptsRDKV/component/dshal/DSHal_GetVideoPortHandle_RF.py
+++ framework/web-app/fileStore/testscriptsRDKV/component/dshal/DSHal_GetVideoPortHandle_RF.py
@@ -48,10 +48,7 @@
   <skip>false</skip>
   <!--  -->
   <box_types>
-    <box_type>IPClient-3</box_type>
-    <!--  -->
     <box_type>Hybrid-1</box_type>
-    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDK2.0</rdk_version>
