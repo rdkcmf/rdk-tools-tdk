f0899440c7536ccd6dd9c7aa1e119efc1f8095d7
Swapna S M <swapna_mohanakumari@comcast.com>
2020-02-26 11:57:42 +0000

    RDKTT-1907 : TDK-V Coverage enhancement - DS HAL
    Reason for change: Added stub and scripts for DS HAL module
    Test Procedure: Run DS HAL scripts using TM
    Risks: None
    Signed-off-by: Swapna S M <swapna_mohanakumari@comcast.com>
    
    (cherry picked from commit b3495133f8bd73bc0a06cc9178869ec865570049)
    Change-Id: If2c8e83d745a2efd3152693343a593104600b0e3

--- DSHal_stub/include/DSHalAgent.h
+++ DSHal_stub/include/DSHalAgent.h
@@ -50,6 +50,7 @@ using namespace std;
 
 int vpHandle = 0;
 int apHandle = 0;
+int dispHandle = 0;
 
 class RDKTestAgent;
 class DSHalAgent : public RDKTestStubInterface , public AbstractServer<DSHalAgent>
@@ -60,12 +61,24 @@ class DSHalAgent : public RDKTestStubInterface , public AbstractServer<DSHalAgen
                 {
                     this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetAudioPort", PARAMS_BY_NAME, JSON_STRING,"portType", JSON_INTEGER,"index", JSON_INTEGER, NULL), &DSHalAgent::DSHal_GetAudioPort);
                     this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetVideoPort", PARAMS_BY_NAME, JSON_STRING,"portType", JSON_INTEGER,"index", JSON_INTEGER, NULL), &DSHalAgent::DSHal_GetVideoPort);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetDisplay", PARAMS_BY_NAME, JSON_STRING,"portType", JSON_INTEGER,"index", JSON_INTEGER, NULL), &DSHalAgent::DSHal_GetDisplay);
                     this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetSurroundMode", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_GetSurroundMode);
                     this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetStereoMode", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_GetStereoMode);
                     this->bindAndAddMethod(Procedure("TestMgr_DSHal_SetStereoMode", PARAMS_BY_NAME, JSON_STRING, "stereoMode", JSON_INTEGER,NULL), &DSHalAgent::DSHal_SetStereoMode);
                     this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetAudioEncoding", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_GetAudioEncoding);
                     this->bindAndAddMethod(Procedure("TestMgr_DSHal_IsAudioPortEnabled", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_IsAudioPortEnabled);
                     this->bindAndAddMethod(Procedure("TestMgr_DSHal_EnableAudioPort", PARAMS_BY_NAME, JSON_STRING, "enable", JSON_INTEGER,NULL), &DSHalAgent::DSHal_EnableAudioPort);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_IsDisplayConnected", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_IsDisplayConnected);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_IsDisplaySurround", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_IsDisplaySurround);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetHDCPProtocol", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_GetHDCPProtocol);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetHDCPReceiverProtocol", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_GetHDCPReceiverProtocol);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetHDCPCurrentProtocol", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_GetHDCPCurrentProtocol);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_IsVideoPortEnabled", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_IsVideoPortEnabled);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_EnableVideoPort", PARAMS_BY_NAME, JSON_STRING, "enable", JSON_INTEGER,NULL), &DSHalAgent::DSHal_EnableVideoPort);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetDisplayAspectRatio", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_GetDisplayAspectRatio);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetColorDepth", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_GetColorDepth);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_GetColorSpace", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_GetColorSpace);
+                    this->bindAndAddMethod(Procedure("TestMgr_DSHal_IsVideoPortActive", PARAMS_BY_NAME, JSON_STRING,NULL), &DSHalAgent::DSHal_IsVideoPortActive);
                 }
 
                 //Inherited functions
@@ -78,11 +91,23 @@ class DSHalAgent : public RDKTestStubInterface , public AbstractServer<DSHalAgen
                 //Stub functions
                 void DSHal_GetAudioPort(IN const Json::Value& req, OUT Json::Value& response);
                 void DSHal_GetVideoPort(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_GetDisplay(IN const Json::Value& req, OUT Json::Value& response);
                 void DSHal_GetSurroundMode(IN const Json::Value& req, OUT Json::Value& response);
                 void DSHal_GetStereoMode(IN const Json::Value& req, OUT Json::Value& response);
                 void DSHal_SetStereoMode(IN const Json::Value& req, OUT Json::Value& response);
                 void DSHal_GetAudioEncoding(IN const Json::Value& req, OUT Json::Value& response);
                 void DSHal_IsAudioPortEnabled(IN const Json::Value& req, OUT Json::Value& response);
                 void DSHal_EnableAudioPort(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_IsDisplayConnected(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_IsDisplaySurround(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_GetHDCPProtocol(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_GetHDCPReceiverProtocol(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_GetHDCPCurrentProtocol(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_IsVideoPortEnabled(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_EnableVideoPort(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_GetDisplayAspectRatio(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_GetColorDepth(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_GetColorSpace(IN const Json::Value& req, OUT Json::Value& response);
+                void DSHal_IsVideoPortActive(IN const Json::Value& req, OUT Json::Value& response);
 };
 #endif //__DSHAL_STUB_H__
--- DSHal_stub/src/DSHalAgent.cpp
+++ DSHal_stub/src/DSHalAgent.cpp
@@ -50,6 +50,7 @@ std::string DSHalAgent::testmodulepre_requisites()
         dsMgr_init();
         dsAudioPortInit();
         dsVideoPortInit();
+        dsDisplayInit();
 
         DEBUG_PRINT(DEBUG_TRACE, "DSHal testmodule pre_requisites --> Exit\n");
         return "SUCCESS";
@@ -75,6 +76,10 @@ bool DSHalAgent::testmodulepost_requisites()
     dsMgr_term();
     dsAudioPortTerm();
     dsVideoPortTerm();
+    dsDisplayTerm();
+    vpHandle = 0;
+    apHandle = 0;
+    dispHandle = 0;
     
     ret = IARM_Bus_Disconnect();
     if(ret == 0)
@@ -200,6 +205,43 @@ void DSHalAgent::DSHal_GetAudioPort(IN const Json::Value& req, OUT Json::Value& 
     }
 }
 /***************************************************************************
+ *Function name : DSHal_GetDisplay
+ *Description    : This function is to get the handle of the video display device
+ *****************************************************************************/
+void DSHalAgent::DSHal_GetDisplay(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetDisplay --->Entry\n");
+    if(&req["portType"] == NULL || &req["index"] == NULL)
+    {
+        return;
+    }
+
+    dsVideoPortType_t portType = (dsVideoPortType_t) req["portType"].asInt();
+    int index = req["index"].asInt();
+
+
+    dsError_t ret = dsERR_NONE;
+    ret = dsGetDisplay(portType, index, &dispHandle);
+
+    if (ret == dsERR_NONE and dispHandle)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = "Display handle retrieved";
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_GetDisplay call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetDisplay -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "Display handle not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_GetDisplay call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetDisplay -->Exit\n");
+        return;
+    }
+}
+
+/***************************************************************************
  *Function name : DSHal_GetSurroundMode
  *Description    : This function is to get the surround mode of the video port
  *****************************************************************************/
@@ -208,7 +250,7 @@ void DSHalAgent::DSHal_GetSurroundMode(IN const Json::Value& req, OUT Json::Valu
     DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetSurroundMode --->Entry\n");
 
     dsError_t ret = dsERR_NONE;
-    int surroundMode = dsSURROUNDMODE_NONE;
+    int surroundMode;
     ret = dsGetSurroundMode(vpHandle, &surroundMode);
 
     if (ret == dsERR_NONE)
@@ -219,9 +261,10 @@ void DSHalAgent::DSHal_GetSurroundMode(IN const Json::Value& req, OUT Json::Valu
         DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetSurroundMode -->Exit\n");
         return;
     }
-    if (ret != dsERR_NONE) {
+    else
+    {
         response["result"] = "FAILURE";
-        response["details"] = dsSURROUNDMODE_NONE;
+        response["details"] = "Surround mode not retrieved";
         DEBUG_PRINT(DEBUG_ERROR, "DSHal_GetSurroundMode call is FAILURE");
         DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetSurroundMode -->Exit\n");
         return;
@@ -263,7 +306,7 @@ void DSHalAgent::DSHal_GetStereoMode(IN const Json::Value& req, OUT Json::Value&
  *****************************************************************************/
 void DSHalAgent::DSHal_SetStereoMode(IN const Json::Value& req, OUT Json::Value& response)
 {
-    DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetStereoMode --->Entry\n");
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_SetStereoMode --->Entry\n");
     if(&req["stereoMode"] == NULL)
     {
         return;
@@ -382,6 +425,341 @@ void DSHalAgent::DSHal_EnableAudioPort(IN const Json::Value& req, OUT Json::Valu
         response["details"] = "EnableAudioPort call failed";
         DEBUG_PRINT(DEBUG_ERROR, "DSHal_EnableAudioPort call is FAILURE");
         DEBUG_PRINT(DEBUG_TRACE, "DSHal_EnableAudioPort -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_IsDisplayConnected
+ *Description    : This function is to find out whether the video port is connected to a display or not
+ *****************************************************************************/
+void DSHalAgent::DSHal_IsDisplayConnected(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsDisplayConnected --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    bool connected;
+
+    ret = dsIsDisplayConnected(vpHandle, &connected);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = connected;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_IsDisplayConnected call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsDisplayConnected -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "Display connection status not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_IsDisplayConnected call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsDisplayConnected -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_IsDisplaySurround
+ *Description    : This function is to check if the connected sink device supports surround
+ *****************************************************************************/
+void DSHalAgent::DSHal_IsDisplaySurround(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsDisplaySurround --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    bool surround;
+
+    ret = dsIsDisplaySurround(vpHandle, &surround);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = surround;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_IsDisplaySurround call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsDisplaySurround -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "Display surround status not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_IsDisplaySurround call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsDisplaySurround -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_GetHDCPProtocol
+ *Description    : This function is to get the STB HDCP protocol version
+ *****************************************************************************/
+void DSHalAgent::DSHal_GetHDCPProtocol(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetHDCPProtocol --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    dsHdcpProtocolVersion_t protocolVersion;
+
+    ret = dsGetHDCPProtocol(vpHandle, &protocolVersion);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = protocolVersion;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_GetHDCPProtocol call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetHDCPProtocol -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "HDCP protocol version not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_GetHDCPProtocol call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetHDCPProtocol -->Exit\n");
+        return;
+    }
+}
+
+/***************************************************************************
+ *Function name : DSHal_GetHDCPReceiverProtocol
+ *Description    : This function is to get the Receiver/TV HDCP protocol version
+ *****************************************************************************/
+void DSHalAgent::DSHal_GetHDCPReceiverProtocol(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetHDCPReceiverProtocol --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    dsHdcpProtocolVersion_t protocolVersion;
+
+    ret = dsGetHDCPReceiverProtocol(vpHandle, &protocolVersion);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = protocolVersion;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_GetHDCPReceiverProtocol call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetHDCPReceiverProtocol -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "HDCP receiver protocol version not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_GetHDCPReceiverProtocol call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetHDCPReceiverProtocol -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_GetHDCPCurrentProtocol
+ *Description    : This function is to get the current used HDCP protocol version
+ *****************************************************************************/
+void DSHalAgent::DSHal_GetHDCPCurrentProtocol(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetHDCPCurrentProtocol --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    dsHdcpProtocolVersion_t protocolVersion;
+
+    ret = dsGetHDCPCurrentProtocol(vpHandle, &protocolVersion);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = protocolVersion;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_GetHDCPCurrentProtocol call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetHDCPCurrentProtocol -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "HDCP receiver protocol version not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_GetHDCPCurrentProtocol call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetHDCPCurrentProtocol -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_IsVideoPortEnabled
+ *Description    : This function is to indicate whether the specified Video port is enabled or not
+ *****************************************************************************/
+void DSHalAgent::DSHal_IsVideoPortEnabled(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsVideoPortEnabled --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    bool enabled;
+
+    ret = dsIsVideoPortEnabled(vpHandle, &enabled);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = enabled;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_IsVideoPortEnabled call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsVideoPortEnabled -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "Audio port enable status not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_IsVideoPortEnabled call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsVideoPortEnabled -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_EnableVideoPort
+ *Description    : This function is to enable or disable the specified Video port
+ *****************************************************************************/
+void DSHalAgent::DSHal_EnableVideoPort(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_EnableVideoPort--->Entry\n");
+    if(&req["enable"] == NULL)
+    {
+        return;
+    }
+
+    bool enable = req["enable"].asInt();
+    dsError_t ret = dsERR_NONE;
+
+    ret = dsEnableVideoPort(vpHandle, enable);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = "EnableVideoPort call success";
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_EnableVideoPort call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_EnableVideoPort -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "EnableVideoPort call failed";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_EnableVideoPort call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_EnableVideoPort -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_GetDisplayAspectRatio
+ *Description    : This function is to get the aspect ratio of the video display
+ *****************************************************************************/
+void DSHalAgent::DSHal_GetDisplayAspectRatio(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetDisplayAspectRatio --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    dsVideoAspectRatio_t aspect;
+
+    ret = dsGetDisplayAspectRatio(dispHandle, &aspect);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = aspect;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_GetDisplayAspectRatio call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetDisplayAspectRatio -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "Display aspect ratio not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_GetDisplayAspectRatio call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetDisplayAspectRatio -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_GetColorDepth
+ *Description    : This function is to get the color depth value
+ *****************************************************************************/
+void DSHalAgent::DSHal_GetColorDepth(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetColorDepth --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    unsigned int depth;
+
+    ret = dsGetColorDepth(vpHandle, &depth);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = depth;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_GetColorDepth call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetColorDepth -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "Color depth value not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_GetColorDepth call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetColorDepth -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_GetColorSpace
+ *Description    : This function is to get the color space value
+ *****************************************************************************/
+void DSHalAgent::DSHal_GetColorSpace(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetColorSpace --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    dsDisplayColorSpace_t space;
+
+    ret = dsGetColorSpace(vpHandle, &space);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = space;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_GetColorSpace call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetColorSpace -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "Color space value not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_GetColorSpace call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_GetColorSpace -->Exit\n");
+        return;
+    }
+}
+/***************************************************************************
+ *Function name : DSHal_IsVideoPortActive
+ *Description    : This function is to find out whether a video port is connected to the active port of sink device
+ *****************************************************************************/
+void DSHalAgent::DSHal_IsVideoPortActive(IN const Json::Value& req, OUT Json::Value& response)
+{
+    DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsVideoPortActive --->Entry\n");
+
+    dsError_t ret = dsERR_NONE;
+    bool active;
+
+    ret = dsIsVideoPortActive(vpHandle, &active);
+
+    if (ret == dsERR_NONE)
+    {
+        response["result"] = "SUCCESS";
+        response["details"] = active;
+        DEBUG_PRINT(DEBUG_LOG, "DSHal_IsVideoPortActive call is SUCCESS");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsVideoPortActive -->Exit\n");
+        return;
+    }
+    else
+    {
+        response["result"] = "FAILURE";
+        response["details"] = "Video port active status not retrieved";
+        DEBUG_PRINT(DEBUG_ERROR, "DSHal_IsVideoPortActive call is FAILURE");
+        DEBUG_PRINT(DEBUG_TRACE, "DSHal_IsVideoPortActive -->Exit\n");
         return;
     }
 }
