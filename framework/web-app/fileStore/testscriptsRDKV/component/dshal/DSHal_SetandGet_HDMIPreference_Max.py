22ad82e3209946471b59309376fa851b42e7e30d
Swapna S M <swapna_mohanakumari@comcast.com>
2020-04-08 07:07:52 +0000

    RDKTT-1951 : DSHal coverage improvement
    Reason for change: Added stub and scripts for DS HAL module
    Test Procedure: Run DS HAL scripts using TM
    Risks: None
    Signed-off-by: Swapna S M <swapna_mohanakumari@comcast.com>
    Change-Id: I4615da529393e288727c939440ec8d03b2f44d9e

--- framework/web-app/fileStore/testscriptsRDKV/component/dshal/DSHal_SetandGet_HDMIPreference_Max.py
+++ framework/web-app/fileStore/testscriptsRDKV/component/dshal/DSHal_SetandGet_HDMIPreference_Max.py
@@ -23,7 +23,7 @@
   <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
   <version>2</version>
   <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
-  <name>DSHal_SetandGet_HDMIPreference_MAX</name>
+  <name>DSHal_SetandGet_HDMIPreference_Max</name>
   <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
   <primitive_test_id></primitive_test_id>
   <!-- Do not change primitive_test_id if you are editing an existing script. -->
@@ -82,7 +82,7 @@ hdcpCurrentProtocol - HDCP protocol version</input_parameters>
 Checkpoint 2 Verify that the HDCP protocol version is set</expected_output>
     <priority>High</priority>
     <test_stub_interface>libdshalstub.so.0.0.0</test_stub_interface>
-    <test_script>DSHal_SetandGet_HDMIPreference_MAX</test_script>
+    <test_script>DSHal_SetandGet_HDMIPreference_Max</test_script>
     <skipped>No</skipped>
     <release_version>M75</release_version>
     <remarks></remarks>
@@ -101,7 +101,7 @@ dshalObj = tdklib.TDKScriptingLibrary("dshal","1");
 #This will be replaced with correspoing Box Ip and port while executing script
 ip = <ipaddress>
 port = <port>
-dshalObj.configureTestCase(ip,port,'DSHal_SetandGet_HDMIPreference_MAX');
+dshalObj.configureTestCase(ip,port,'DSHal_SetandGet_HDMIPreference_Max');
 
 #Get the result of connection with test component and STB
 dshalloadModuleStatus = dshalObj.getLoadModuleResult();
