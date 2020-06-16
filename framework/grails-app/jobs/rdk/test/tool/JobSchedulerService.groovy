119f55078c2da470c77ffafcefb6c934814433db
nneela690 <Neerada_Neelakantan@comcast.com>
2020-06-16 20:53:30 +0530

    RDKTT-2109: Change time to 24h format in scriptlog
    Reason for change: Change time to 24hour format in script log
    Test Procedure: with TDK TM
    Risks: Low
    Signed-off-by: neerada.n <neerada.n@tataelxsi.co.in>
    
    (cherry picked from commit dba43984ef725ee7337fd2e4d675290abc6087fc)
    Change-Id: Id7ba62d8cd05bd11ce84af722d2c95cbb73bb369

--- framework/grails-app/jobs/rdk/test/tool/JobSchedulerService.groovy
+++ framework/grails-app/jobs/rdk/test/tool/JobSchedulerService.groovy
@@ -2454,7 +2454,7 @@ class JobSchedulerService implements Job{
 		return output
 	}
 	def getCurrentTime(){
-		SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss")
+		SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss")
 		String timeString = format.format(new Date())
 		return timeString
 	}
