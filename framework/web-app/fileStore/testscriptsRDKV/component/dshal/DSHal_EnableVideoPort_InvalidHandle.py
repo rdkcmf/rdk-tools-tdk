16fd384018ba53bc2bccf831cf64afb17d7fc37c
Swapna S M <swapna_mohanakumari@comcast.com>
2020-08-24 11:08:54 +0000

    RDKTT-2279 : DSHal coverage improvement
    Reason for change: Added new stub and scripts for DS HAL module
    Test Procedure: Run DS HAL scripts using TM
    Risks: None
    Signed-off-by: Swapna S M <swapna_mohanakumari@comcast.com>
    Change-Id: I118be6c7ccfd7961960c75d3e18023a59a09e5c1

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/dshal/DSHal_EnableVideoPort_InvalidHandle.py
@@ -0,0 +1,144 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>2</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>DSHal_EnableVideoPort_InvalidHandle</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>DSHal_EnableVideoPort</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To check if the video port enable status is set for invalid handle</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>2</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>IPClient-3</box_type>
+    <!--  -->
+    <box_type>Hybrid-1</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>CT_DS_HAL_148</test_case_id>
+    <test_objective>To check if the video port enable status is set for invalid handle</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>XG1V3,XI3</test_setup>
+    <pre_requisite>1. Initialize IARMBus
+2. Connect IARMBus
+3. Initialize dsMgr
+4. Initialize DSHAL subsystems</pre_requisite>
+    <api_or_interface_used>dsGetVideoPort(dsVideoPortType_t type, int index, int *handle)
+dsEnableVideoPort(int handle, bool enabled)</api_or_interface_used>
+    <input_parameters>type - Video port type
+index- Video port index
+handle - Video port handle
+enabled - enable status to set</input_parameters>
+    <automation_approch>1. TM loads the DSHAL agent via the test agent.
+2 . DSHAL agent will invoke the api dsGetVideoPort to get the handle for invalid handle
+3 . DSHAL agent will invoke the api dsEnableVideoPort to set the video port enable status
+4. TM checks if the API call fails and return SUCCESS/FAILURE status.</automation_approch>
+    <expected_output>Checkpoint 1.Verify the API call is failure</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>libdshalstub.so.0.0.0</test_stub_interface>
+    <test_script>DSHal_EnableVideoPort_InvalidHandle</test_script>
+    <skipped>No</skipped>
+    <release_version>M80</release_version>
+    <remarks></remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib; 
+from dshalUtility import *;
+
+#Test component to be tested
+dshalObj = tdklib.TDKScriptingLibrary("dshal","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+dshalObj.configureTestCase(ip,port,'DSHal_EnableVideoPort_InvalidHandle');
+
+#Get the result of connection with test component and STB
+dshalloadModuleStatus = dshalObj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %dshalloadModuleStatus;
+
+dshalObj.setLoadModuleStatus(dshalloadModuleStatus);
+
+if "SUCCESS" in dshalloadModuleStatus.upper():
+    expectedResult = "FAILURE";
+    #Prmitive test case which associated to this Script
+    tdkTestObj = dshalObj.createTestStep('DSHal_GetVideoPort');
+    tdkTestObj.addParameter("portType", videoPortType["INVALID"]);
+    #Execute the test case in STB
+    tdkTestObj.executeTestCase(expectedResult);
+    actualResult = tdkTestObj.getResult();
+    print "DSHal_GetVideoPort result: ", actualResult
+
+    if expectedResult in actualResult:
+        tdkTestObj.setResultStatus("SUCCESS");
+        details = tdkTestObj.getResultDetails();
+        print details;
+
+        tdkTestObj = dshalObj.createTestStep('DSHal_EnableVideoPort');
+        #Execute the test case in STB
+        tdkTestObj.executeTestCase(expectedResult);
+        actualResult = tdkTestObj.getResult();
+        print "DSHal_EnableVideoPort result: ", actualResult;
+        details = tdkTestObj.getResultDetails();
+        print "Video port enable status: ", details
+        if expectedResult in actualResult:
+            tdkTestObj.setResultStatus("SUCCESS");
+            print "Video port enable status not set for invalid hanle which is expected";
+        else:
+            tdkTestObj.setResultStatus("FAILURE");
+            print "Video port enable status set for invalid handle which is not expected";
+    else:
+        tdkTestObj.setResultStatus("FAILURE");
+        print "Video port handle retrieved for invalid port type";
+
+    dshalObj.unloadModule("dshal");
+
+else:
+    print "Module load failed";
