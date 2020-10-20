533957334d4ac08c9c78f88a86e9fd8902c34cef
vkutta001c <Vimal_KuttanPillai@comcast.com>
2020-10-20 06:14:22 +0000

    RDKTT-2405: RDK Service Coverage Enhancements
    Reason for change: Added test scripts for RDK Service test coverage
    Test Procedure: Test with TDK TM
    Risks: Low
    Signed-off-by: Vimalks<vimalks@tataelxsi.co.in>
    Change-Id: I8988c2e3c0f2608a50b36159de9a1126cbace0c0

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/rdkservices/RdkService_Warehouse.py
@@ -0,0 +1,111 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>RdkService_Warehouse</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>RdkService_Test</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>3</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To test RDK service Warehouse api's</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>5</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>RPI-Client</box_type>
+    <!--  -->
+    <box_type>RPI-HYB</box_type>
+    <!--  -->
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id></test_case_id>
+    <test_objective></test_objective>
+    <test_type></test_type>
+    <test_setup></test_setup>
+    <pre_requisite></pre_requisite>
+    <api_or_interface_used></api_or_interface_used>
+    <input_parameters></input_parameters>
+    <automation_approch></automation_approch>
+    <expected_output></expected_output>
+    <priority></priority>
+    <test_stub_interface></test_stub_interface>
+    <test_script></test_script>
+    <skipped></skipped>
+    <release_version></release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib; 
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("rdkservices","1",standAlone=True);
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'RdkService_Warehouse');
+
+#Get the result of connection with test component and DUT
+result =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %result;
+
+#Prmitive test case which associated to this Script
+tdkTestObj = obj.createTestStep('RdkService_Test');
+tdkTestObj.addParameter("xml_name","Warehouse");
+expectedResult = "SUCCESS"
+#Execute the test case in DUT
+tdkTestObj.executeTestCase(expectedResult);
+
+#Get the result of execution
+result = tdkTestObj.getResult();
+print "[TEST EXECUTION RESULT] : %s" %result;
+
+#Set the result status of execution
+tdkTestObj.setResultStatus(result);
+
+obj.unloadModule("rdkservices");
