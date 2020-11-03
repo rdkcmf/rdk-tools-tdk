2f1fe54b3012eeff62f1f0f2fe86b1d08e950165
Abin T S <Abin_thirukkulamshaju@comcast.com>
2020-11-03 12:27:10 +0000

    RDKTT-2427 : Browser Performance Tests
    Reason for change: adding test script and and variables for automating html5 and sunspider tests
    Test Procedure: Check if test success with TDK
    Risks: None
    Signed-off-by:Abin T S<abin.ts@tataelxsi.co.in>
    
    (cherry picked from commit a8a8f678cf4004fb7333da00cbfc244c6cdd913e)
    Change-Id: Iaf441f07400cc934c4c9728c5a8e606b4a02bc48

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/rdkservices/RdkService_BrowserPerformance_HTML5.py
@@ -0,0 +1,190 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>1</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>RdkService_BrowserPerformance_HTML5</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>rdkservice_getBrowserScore_HTML5</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To get the browser score using HTML5 test</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>4</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>RPI-Client</box_type>
+    <!--  -->
+    <box_type>RPI-HYB</box_type>
+    <!--  -->
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id></test_case_id>
+    <test_objective></test_objective>
+    <test_type></test_type>
+    <test_setup></test_setup>
+    <pre_requisite></pre_requisite>
+    <api_or_interface_used></api_or_interface_used>
+    <input_parameters></input_parameters>
+    <automation_approch></automation_approch>
+    <expected_output></expected_output>
+    <priority></priority>
+    <test_stub_interface></test_stub_interface>
+    <test_script></test_script>
+    <skipped></skipped>
+    <release_version></release_version>
+    <remarks></remarks>
+  </test_cases>
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script 
+import tdklib; 
+from BrowserPerformanceUtility import *
+import BrowserPerformanceUtility
+from performancelib import *
+import performancelib
+import BrowserPerformanceVariables
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("rdkservices","1",standAlone=True)
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'RdkService_BrowserPerformance_HTML5')
+
+#Get the result of connection with test component and DUT
+result =obj.getLoadModuleResult()
+print "[LIB LOAD STATUS]  :  %s" %result
+
+expectedResult = "SUCCESS"
+if expectedResult in result.upper():
+    browser_test_url=BrowserPerformanceVariables.html5_test_url
+    print "Check Pre conditions"
+    #No need to revert any values if the pre conditions are already set.
+    revert="NO"
+    status,curr_ux_status,curr_webkit_status,curr_cobalt_status = check_pre_requisites(obj)
+    print "Current values \nUX:%s\nWebKitBrowser:%s\nCobalt:%s"%(curr_ux_status,curr_webkit_status,curr_cobalt_status);
+    if status == "FAILURE":
+        set_pre_requisites(obj)
+	#Need to revert the values since we are changing plugin status
+        revert="YES"
+        status,ux_status,webkit_status,cobalt_status = check_pre_requisites(obj)
+    if status == "SUCCESS":
+        print "\nPre conditions for the test are set successfully";
+        print "\nGet the URL in WebKitBrowser"
+        tdkTestObj = obj.createTestStep('rdkservice_getValue');
+        tdkTestObj.addParameter("method","WebKitBrowser.1.url");
+        tdkTestObj.executeTestCase(expectedResult);
+        current_url = tdkTestObj.getResultDetails();
+        if current_url != None:
+            tdkTestObj.setResultStatus("SUCCESS");
+            print "Current URL:",current_url
+            print "\nSet HTML5 test URL"
+            tdkTestObj = obj.createTestStep('rdkservice_setValue');
+            tdkTestObj.addParameter("method","WebKitBrowser.1.url");
+            tdkTestObj.addParameter("value",browser_test_url);
+            tdkTestObj.executeTestCase(expectedResult);
+            result = tdkTestObj.getResult();
+            time.sleep(10)
+            print "\nValidate if the URL is set successfully or not"
+            tdkTestObj = obj.createTestStep('rdkservice_getValue');
+            tdkTestObj.addParameter("method","WebKitBrowser.1.url");
+            tdkTestObj.executeTestCase(expectedResult);
+            new_url = tdkTestObj.getResultDetails();
+            if new_url == browser_test_url:
+                tdkTestObj.setResultStatus("SUCCESS");
+                print "URL(",new_url,") is set successfully"
+                time.sleep(20)
+	    	tdkTestObj = obj.createTestStep('rdkservice_getBrowserScore_HTML5');
+                tdkTestObj.executeTestCase(expectedResult);
+                browser_score = tdkTestObj.getResultDetails();
+                if browser_score != "Unable to get the browser score":
+                    tdkTestObj.setResultStatus("SUCCESS")
+		    browser_score = int(browser_score.split()[0])
+		    print "\n validating browser score with threshold value:"
+		    conf_file,result = getConfigFileName(tdkTestObj.realpath)
+                    result, html5_threshold_value = getDeviceConfigKeyValue(conf_file,"HTML5_THRESHOLD_VALUE")
+                    if result == "SUCCESS":
+                        if int(browser_score) > int(html5_threshold_value):
+                            tdkTestObj.setResultStatus("SUCCESS");
+                            print "The browser performance is high as expected"
+                        else:
+                            tdkTestObj.setResultStatus("FAILURE");
+                            print "The browser performance is lower than expected"
+                    else:
+                        tdkTestObj.setResultStatus("FAILURE");
+                        print "Failed to get the threshold value from config file"
+                else:
+                    tdkTestObj.setResultStatus("FAILURE")
+		    print "Failed to get the browser score"
+            else:
+                print "Failed to load the URL",new_url
+                tdkTestObj.setResultStatus("FAILURE");
+            #Set the URL back to previous
+            tdkTestObj = obj.createTestStep('rdkservice_setValue');
+            tdkTestObj.addParameter("method","WebKitBrowser.1.url");
+            tdkTestObj.addParameter("value",current_url);
+            tdkTestObj.executeTestCase(expectedResult);
+            result = tdkTestObj.getResult();
+            if result == "SUCCESS":
+                print "URL is reverted successfully"
+                tdkTestObj.setResultStatus("SUCCESS");
+            else:
+                print "Failed to revert the URL"
+                tdkTestObj.setResultStatus("FAILURE");
+        else:
+            tdkTestObj.setResultStatus("FAILURE");
+            print "Failed to get URL from webkitbrowser"
+    else:
+        print "Pre conditions are not met"
+    #Revert the values
+    if revert=="YES":
+        print "Revert the values before exiting"
+        status = revert_value(curr_ux_status,curr_webkit_status,curr_cobalt_status,obj);
+    obj.unloadModule("rdkservices");
+else:
+    obj.setLoadModuleStatus("FAILURE");
+    print "Failed to load module"
