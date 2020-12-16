f54f1784ff7e4030ac7154fe711aa0af32523fa2
Abin T S <Abin_thirukkulamshaju@comcast.com>
2020-12-16 18:56:10 +0000

    RDKTT-2544 : Stability and Performance enhancements
    Reason for change: Add support for dynamically taking device IP, add support for skipping ad in cobalt
    Test Procedure: Check if test success with TDK
    Risks: None
    Signed-off-by:Abin T S<abin.ts@tataelxsi.co.in>
    
    (cherry picked from commit 008c94ef9a779b742ce2b6c834e4830b07231a2e)
    Change-Id: Id5563826b2a47bb3582569042eebaa7d6150d899

--- framework/web-app/fileStore/testscriptsRDKV/component/rdkv_performance/RdkService_FunctionalPerformance_UILaunchTime.py
+++ framework/web-app/fileStore/testscriptsRDKV/component/rdkv_performance/RdkService_FunctionalPerformance_UILaunchTime.py
@@ -21,7 +21,7 @@
 <xml>
   <id></id>
   <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
-  <version>2</version>
+  <version>5</version>
   <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
   <name>RdkService_FunctionalPerformance_UILaunchTime</name>
   <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
@@ -37,7 +37,7 @@
   <!--  -->
   <groups_id />
   <!--  -->
-  <execution_time>6</execution_time>
+  <execution_time>10</execution_time>
   <!--  -->
   <long_duration>false</long_duration>
   <!--  -->
