03ff23dde421e7573f50b7581e8155355df3719e
Abin T S <Abin_thirukkulamshaju@comcast.com>
2021-01-05 11:08:35 +0000

    RDKTT-2586 : App performance validation enhancement
    Reason for change: enhance existing scripts and add script to find cpu load and memory usage
    Test Procedure: Check if test success with TDK
    Risks: None
    Signed-off-by:Abin T S<abin.ts@tataelxsi.co.in>
    
    Change-Id: I304bc30b6082a71fdf4b13c00e41c45e43d0e16b

--- framework/web-app/fileStore/testscriptsRDKV/component/rdkv_performance/RdkService_AppPerformance_CPUMem_Usage.py
+++ framework/web-app/fileStore/testscriptsRDKV/component/rdkv_performance/RdkService_AppPerformance_CPUMem_Usage.py
@@ -2,7 +2,7 @@
 # If not stated otherwise in this file or this component's Licenses.txt
 # file the following copyright and licenses apply:
 #
-# Copyright 2020 RDK Management
+# Copyright 2021 RDK Management
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,73 +20,71 @@
 <?xml version="1.0" encoding="UTF-8"?><xml>
   <id/>
   <version>1</version>
-  <name>RdkService_AppPerformance_AppLaunch</name>
+  <name>RdkService_AppPerformance_CPUMem_Usage</name>
   <primitive_test_id/>
   <primitive_test_name>rdkservice_setValue</primitive_test_name>
   <primitive_test_version>1</primitive_test_version>
   <status>FREE</status>
-  <synopsis>This script is to get the time taken to launch the lightning application with webkit</synopsis>
+  <synopsis>The objective of this test is to get the CPU load and Memory usage while launching an application</synopsis>
   <groups_id/>
-  <execution_time>4</execution_time>
+  <execution_time>5</execution_time>
   <long_duration>false</long_duration>
   <advanced_script>false</advanced_script>
   <remarks/>
   <skip>false</skip>
   <box_types>
-    <box_type>RPI-HYB</box_type>
     <box_type>RPI-Client</box_type>
+    <box_type>RPI-HYB</box_type>
     <box_type>Video_Accelerator</box_type>
   </box_types>
-  <rdk_versions>
-    <rdk_version>RDK2.0</rdk_version>
-  </rdk_versions>
   <test_cases>
-    <test_case_id>RDKV_PERFORMANCE_02</test_case_id>
-    <test_objective>This script is to get the time taken to launch the lightning application with webkit</test_objective>
+    <test_case_id>RDKV_PERFORMANCE_17</test_case_id>
+    <test_objective>The objective of this test is to get the CPU and Memory usage while launching an application</test_objective>
     <test_type>Positive</test_type>
-    <test_setup>RPI, Accelerator</test_setup>
-    <pre_requisite>1. Wpeframework process should be up and running in the device.
-2. Test Manager time should be in sync with UTC time.
-</pre_requisite>
+    <test_setup>RPI,Accelerator</test_setup>
+    <pre_requisite>1. Wpeframework process should be up and running in the device.</pre_requisite>
     <api_or_interface_used>None</api_or_interface_used>
     <input_parameters>1. The URL of the application to be launched.
 </input_parameters>
     <automation_approch>1. As a pre requisite disable all other plugins and enable webkitbrowser plugin.
 2. Set the application URL in webkitbrowser
-3. Get the time taken to load the application</automation_approch>
-    <expected_output>The application should launch within 60 seconds.</expected_output>
+3. Get the cpu load and memory usage after launching the application
+4. Revert the status of plugins</automation_approch>
+    <expected_output>CPU load and memory usage must be within the expected range.</expected_output>
     <priority>High</priority>
     <test_stub_interface>rdkv_performance</test_stub_interface>
-    <test_script>RdkService_AppPerformance_AppLaunch</test_script>
+    <test_script>RdkService_AppPerformance_CPUMem_Usage</test_script>
     <skipped>No</skipped>
-    <release_version>M82</release_version>
+    <release_version>M84</release_version>
     <remarks/>
   </test_cases>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+  </rdk_versions>
 </xml>
 
 '''
-# use tdklib library,which provides a wrapper for tdk testcase script 
+ # use tdklib library,which provides a wrapper for tdk testcase script 
 import tdklib; 
 from web_socket_util import *
 import MediaValidationVariables
 from MediaValidationUtility import *
 from StabilityTestUtility import *
-from datetime import datetime
+from rdkv_performancelib import *
+
 
 #Test component to be tested
-obj = tdklib.TDKScriptingLibrary("rdkv_performance","1",standAlone=True)
+obj = tdklib.TDKScriptingLibrary("rdkv_performance","1",standAlone=True);
 
 #IP and Port of box, No need to change,
 #This will be replaced with corresponding DUT Ip and port while executing script
 ip = <ipaddress>
 port = <port>
-obj.configureTestCase(ip,port,'RdkService_AppPerformance_AppLaunch');
-
-webkit_console_socket = None
+obj.configureTestCase(ip,port,'RdkService_AppPerformance_CPUMem_Usage');
 
 #Get the result of connection with test component and DUT
 result =obj.getLoadModuleResult();
-print "[LIB LOAD STATUS]  :  %s" %result
+print "[LIB LOAD STATUS]  :  %s" %result;
 obj.setLoadModuleStatus(result);
 
 expectedResult = "SUCCESS"
@@ -103,6 +101,7 @@ if expectedResult in result.upper():
     appArguments = getURLArguments()
     # Getting the complete test app URL
     video_test_url = getTestURL(appURL,appArguments)
+
     print "Check Pre conditions"
     #No need to revert any values if the pre conditions are already set.
     revert="NO"
@@ -125,12 +124,11 @@ if expectedResult in result.upper():
             tdkTestObj.setResultStatus("SUCCESS");
             webkit_console_socket = createEventListener(ip,MediaValidationVariables.webinspect_port,[],"/devtools/page/1",False)
             time.sleep(10)
-            print "Current URL:",current_url
+            print "\nCurrent URL:",current_url
             print "\nSet Lightning Application URL"
             tdkTestObj = obj.createTestStep('rdkservice_setValue');
             tdkTestObj.addParameter("method","WebKitBrowser.1.url");
             tdkTestObj.addParameter("value",video_test_url);
-	    start_time = str(datetime.utcnow()).split()[1]
             tdkTestObj.executeTestCase(expectedResult);
             result = tdkTestObj.getResult();
             print "\nValidate if the URL is set successfully or not"
@@ -140,7 +138,7 @@ if expectedResult in result.upper():
             new_url = tdkTestObj.getResultDetails();
             if new_url in video_test_url:
                 tdkTestObj.setResultStatus("SUCCESS");
-                print "URL(",new_url,") is set successfully"
+                print "\n URL(",new_url,") is set successfully \n"
                 continue_count = 0
                 test_result = ""
                 while True:
@@ -157,29 +155,51 @@ if expectedResult in result.upper():
                         break;
                 webkit_console_socket.disconnect()
                 if "URL Info:" in test_result:
-                    micosec_frm_start_time = int(start_time.split(".")[-1])
-                    start_time = start_time.replace(start_time.split(".")[-1],"")
-                    start_time = start_time.replace(".",":")
-                    start_time = start_time + str(micosec_frm_start_time/1000)
-                    print "\nApplication URL set in webkit browser at :{} (UTC)".format(start_time)
-                    start_time_millisec = getTimeInMilliSeconds(start_time)
-                    end_time = getTimeFromMsg(test_result)
-                    print "\nApplication launched at: {} (UTC)".format(end_time)
-                    end_time_millisec = getTimeInMilliSeconds(end_time)
-                    app_launch_time = end_time_millisec - start_time_millisec
-                    print "\nTime taken to launch the application: {} milliseconds \n".format(app_launch_time)
-                    conf_file,result = getConfigFileName(tdkTestObj.realpath)
-                    result, app_launch_threshold_value = getDeviceConfigKeyValue(conf_file,"APP_LAUNCH_THRESHOLD VALUE")
+                    #get the cpu load
+                    print "\n Application launched successfully \n "
+                    tdkTestObj = obj.createTestStep('rdkservice_getCPULoad')
+                    tdkTestObj.executeTestCase(expectedResult)
+                    result = tdkTestObj.getResult()
+                    cpuload = tdkTestObj.getResultDetails()
                     if result == "SUCCESS":
-                        if int(app_launch_time) < int(app_launch_threshold_value):
-                            tdkTestObj.setResultStatus("SUCCESS");
-                            print "\n The time taken to launch the app is within the expected limit\n"
+                        tdkTestObj.setResultStatus("SUCCESS")
+                        #validate the cpuload
+                        tdkTestObj = obj.createTestStep('rdkservice_validateCPULoad')
+                        tdkTestObj.addParameter('value',float(cpuload))
+                        tdkTestObj.addParameter('threshold',90.0)
+                        tdkTestObj.executeTestCase(expectedResult)
+                        is_high_cpuload = tdkTestObj.getResultDetails()
+                        if is_high_cpuload == "YES" :
+                            print "\n cpu load is high :{}%".format(cpuload)
+                            tdkTestObj.setResultStatus("FAILURE")
                         else:
-                            tdkTestObj.setResultStatus("FAILURE");
-                            print "\n The time taken to launch the app is greater than the expected limit \n"
+                            tdkTestObj.setResultStatus("SUCCESS")
+                            print "\n cpu load : {}%\n".format(cpuload)
                     else:
-                        tdkTestObj.setResultStatus("FAILURE");
-                        print "Failed to get the threshold value from config file"
+                        print "Unable to get cpuload"
+                        tdkTestObj.setResultStatus("FAILURE")
+                    #get the memory usage
+                    tdkTestObj = obj.createTestStep('rdkservice_getMemoryUsage')
+                    tdkTestObj.executeTestCase(expectedResult)
+                    result = tdkTestObj.getResult()
+                    memory_usage = tdkTestObj.getResultDetails()
+                    if (result == "SUCCESS"):
+                        tdkTestObj.setResultStatus("SUCCESS")
+                        #validate memory usage
+                        tdkTestObj = obj.createTestStep('rdkservice_validateMemoryUsage')
+                        tdkTestObj.addParameter('value',float(memory_usage))
+                        tdkTestObj.addParameter('threshold',90.0)
+                        tdkTestObj.executeTestCase(expectedResult)
+                        is_high_memory_usage = tdkTestObj.getResultDetails()
+                        if is_high_memory_usage == "YES":
+                            print "\nmemory usage is high :{}%\n".format(memory_usage)
+                            tdkTestObj.setResultStatus("FAILURE")
+                        else:
+                            print "\nmemory usage :{}%\n".format(memory_usage)
+                            tdkTestObj.setResultStatus("SUCCESS")
+                    else:
+                        print "\n Unable to get the memory usage\n"
+                        tdkTestObj.setResultStatus("FAILURE")
                 else:
                     tdkTestObj.setResultStatus("FAILURE");
                     print "error occured during application launch"
@@ -190,7 +210,7 @@ if expectedResult in result.upper():
                 tdkTestObj.executeTestCase(expectedResult);
                 result = tdkTestObj.getResult();
                 if result == "SUCCESS":
-                    print "URL is reverted successfully"
+                    print "URL is reverted successfully \n"
                     tdkTestObj.setResultStatus("SUCCESS");
                 else:
                     print "Failed to revert the URL"
@@ -211,4 +231,3 @@ if expectedResult in result.upper():
 else:
     obj.setLoadModuleStatus("FAILURE");
     print "Failed to load module"
-
