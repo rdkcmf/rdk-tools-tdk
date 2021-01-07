ddfc712b89ad08e00eed2c931992391ebc2e1f71
Rini Gifty <rini_gifty@comcast.com>
2021-01-07 13:53:29 +0000

    RDKTT-2595: RDK Security Validation Scripts
    Reason for change: 1.Adding Security Validation Module and scripts for network port scanning and SSL certificate expiry check
                       2.Added sripts to check  TLS Fallback, Compression, Renegotiation and supported SSL/TLS protocols
    Test Procedure: Tested through Test Manager
    Risks: Low
    Signed-off-by: RiniGifty<rini.g@tataelxsi.co.in>
    Change-Id: If5e18fd8596bced963b5b09670321c9001f51389

---
+++ framework/web-app/fileStore/testscriptsRDKV/certification/rdkv_security/RdkvSecurity_CheckSSLCertificateExpiry.py
@@ -0,0 +1,169 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2021 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>18</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>RdkvSecurity_CheckSSLCertificateExpiry</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id> </primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>rdkvsecurity_executeInDUT</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>Checking if the SSL certificates available in the DUT are expected to expire within the specified time</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>5</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>RPI-Client</box_type>
+    <!--  -->
+    <box_type>RPI-HYB</box_type>
+    <!--  -->
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>RDKV_SECURITY_03</test_case_id>
+    <test_objective>Checking if the SSL Certificates present in DUT are expected to expire within the specified time</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>RPI,Accelerator</test_setup>
+    <pre_requisite>1. openssl tool must be available in the DUT
+2. The path for the SSL certificates folder and the expected expiry period(in seconds) within which to check should be specified in the device.config
+3. Credentials to ssh to the DUT should be available in device.config</pre_requisite>
+    <api_or_interface_used>None</api_or_interface_used>
+    <input_parameters>None</input_parameters>
+    <automation_approch>1. Retrieve the SSH credentials, expiry period and certificate path from device.config
+2. Execute the openssl command to check the expiry of each certificate in the specified folder and return the command output which is a multiline string with the "Certificate will expire"/"Certificate will not expire" string and the name of certificates that are expected to expire along with their expiry date.
+3. From the output string, check if there are any certificates that are expected to expire within the specified time(with "Certificate will expire" string). The test pass/fail based on the absence/presence of "Certificate will expire" string.</automation_approch>
+    <expected_output>The multiline output string should not contain  "Certificate will expire" substring</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>rdkv_security</test_stub_interface>
+    <test_script>RdkvSecurity_CheckSSLCertificateExpiry</test_script>
+    <skipped>No</skipped>
+    <release_version>M85</release_version>
+    <remarks></remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+from rdkv_securitylib import *;
+import rdkv_securitylib;
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("rdkv_security","1",standAlone=True);
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'RdkvSecurity_CheckSSLCertificateExpiry');
+
+#Get the result of connection with test component and DUT
+result =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %result;
+obj.setLoadModuleStatus(result.upper());
+
+expectedResult = "SUCCESS"
+if expectedResult in result.upper():
+    configKeyList = ["EXPIRY_PERIOD" , "CERT_PATH", "SSH_METHOD", "SSH_USERNAME", "SSH_PASSWORD"]
+    configValues = {}
+    tdkTestObj = obj.createTestStep('rdkvsecurity_getDeviceConfig')
+    #Get each configuration from device config file
+    for configKey in configKeyList:
+        tdkTestObj.addParameter("basePath",obj.realpath)
+        tdkTestObj.addParameter("configKey",configKey)
+        tdkTestObj.executeTestCase(expectedResult)
+        configValues[configKey] = tdkTestObj.getResultDetails()
+        if "FAILURE" not in configValues[configKey]:
+            print "Successfully retrieved %s configuration from device config file" %(configKey)
+            tdkTestObj.setResultStatus("SUCCESS")
+        else:
+            print "Failed to retrieve %s configuration from device config file" %(configKey)
+            tdkTestObj.setResultStatus("FAILURE")
+            result = "FAILURE"
+            break
+    if "FAILURE" != result:
+        if "directSSH" == configValues["SSH_METHOD"] :
+            if configValues["SSH_PASSWORD"] == "None":
+                configValues["SSH_PASSWORD"] = ""
+            credentials = obj.IP + ',' + configValues["SSH_USERNAME"] + ',' + configValues["SSH_PASSWORD"]
+            command = 'for pem in '+ configValues["CERT_PATH"] + '/*.pem; do if ! openssl x509 -checkend ' + configValues["EXPIRY_PERIOD"] + ' -in "$pem"; then printf \'%s: %s\\n\' "$(openssl x509 -enddate -noout -in "$pem"|cut -d= -f 2)" "$pem"; fi done'
+            print "Command : %s" %(command)
+
+            #Primitive test case which associated to this Script
+            tdkTestObj = obj.createTestStep('rdkvsecurity_executeInDUT');
+            #Add the parameters to ssh to the DUT and execute the command
+            tdkTestObj.addParameter("sshMethod", configValues["SSH_METHOD"]);
+            tdkTestObj.addParameter("credentials", credentials);
+            tdkTestObj.addParameter("command", command);
+
+            #Execute the test case in DUT
+            tdkTestObj.executeTestCase(expectedResult);
+
+            #Get the result of execution
+            output = tdkTestObj.getResultDetails();
+            if "Certificate will expire" in output:
+                certListToBeExpired = []
+                for line in output.splitlines():
+                    if (".pem" in line) and (line not in command):
+                        certListToBeExpired.append (line)
+                tdkTestObj.setResultStatus("FAILURE");
+                months = int(configValues["EXPIRY_PERIOD"])/2630000
+                print "Test failed.\nFew Certificates are expected to expire within %d months: %s\n" %(months,certListToBeExpired)
+            else:
+                tdkTestObj.setResultStatus("SUCCESS");
+                print "Test passed. No certificate expires within specified time"
+        else:
+            print "Currently only supports directSSH ssh method"
+            tdkTestObj.setResultStatus("FAILURE");
+    else:
+        print "Failed to get configuration values"
+        tdkTestObj.setResultStatus("FAILURE");
+
+    #Unload the module
+    obj.unloadModule("rdkv_security");
+
+else:
+    #Set load module status
+    obj.setLoadModuleStatus("FAILURE");
+    print "Failed to load module"
+
---
+++ framework/web-app/fileStore/testscriptsRDKV/certification/rdkv_security/RdkvSecurity_CheckTLSFallback.py
@@ -0,0 +1,200 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2021 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>14</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>RdkvSecurity_CheckTLSFallback</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>rdkvsecurity_executeInTM</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>Checks the TLS Fallback settings</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>5</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>RPI-Client</box_type>
+    <!--  -->
+    <box_type>RPI-HYB</box_type>
+    <!--  -->
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>RDKV_SECURITY_05</test_case_id>
+    <test_objective>Checks the TLS Fallback settings</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>RPI,Accelerator</test_setup>
+    <pre_requisite>1. Sslscan tool should be installed in the TM machine
+2. Configure the values supported SSL/TLS protocols (variable $SUPPORTED_SSL_TLS_PROTOCOLS), test URL (variable $TEST_URL)and SSLscan path (variable $SSL_SCAN_PATH) available in fileStore/tdkvRDKServiceConfig/device.config file</pre_requisite>
+    <api_or_interface_used>None</api_or_interface_used>
+    <input_parameters>None</input_parameters>
+    <automation_approch>1. Execute the sslscan with parameters to list the protocols and fallback settings of the DUT
+3. If supported SSL/TLS protocols  configured check whether configured  protocols are enabled
+4. Check  if Fallback feature is supported
+5. Pass/fail the test based on the enabled/disabled state of Fallback feature</automation_approch>
+    <expected_output> If supported SSL/TLS protocol configured, Fallback must be enabled</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>rdkv_security</test_stub_interface>
+    <test_script>RdkvSecurity_CheckTLSFallback</test_script>
+    <skipped>No</skipped>
+    <release_version>M85</release_version>
+    <remarks></remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+from rdkv_securitylib import *;
+import rdkv_securitylib;
+
+
+#Test component to be tested
+obj = tdklib.TDKScriptingLibrary("rdkv_security","1",standAlone=True);
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+obj.configureTestCase(ip,port,'RdkvSecurity_CheckTLSFallback');
+
+#Get the result of connection with test component and DUT
+result =obj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %result;
+result = obj.getLoadModuleResult();
+
+expectedResult = "SUCCESS"
+if expectedResult in result.upper():
+    configKeyList = ["SSL_SCAN_PATH","SUPPORTED_SSL_TLS_PROTOCOLS","TEST_WEB_APP_URL"]
+    configValues = {}
+    tdkTestObj = obj.createTestStep('rdkvsecurity_getDeviceConfig')
+    #Get each configuration from device config file
+    for configKey in configKeyList:
+        tdkTestObj.addParameter("basePath",obj.realpath)
+        tdkTestObj.addParameter("configKey",configKey)
+        tdkTestObj.executeTestCase(expectedResult)
+        configValues[configKey] = tdkTestObj.getResultDetails()
+        if "FAILURE" not in configValues[configKey]:
+            print "Successfully retrieved %s configuration from device config file" %(configKey)
+            tdkTestObj.setResultStatus("SUCCESS")
+        else:
+            print "Failed to retrieve %s configuration from device config file" %(configKey)
+            tdkTestObj.setResultStatus("FAILURE")
+            result = "FAILURE"
+            break
+    if "FAILURE" != result:
+         tdkTestObj = obj.createTestStep('rdkvsecurity_executeInTM');
+         command = configValues["SSL_SCAN_PATH"]+"/sslscan "+configValues["TEST_WEB_APP_URL"]
+         tdkTestObj.addParameter("command", command);
+         tdkTestObj.executeTestCase(expectedResult);
+         Result = tdkTestObj.getResultDetails()
+         if "FAILURE" not in Result:
+             tdkTestObj.setResultStatus("SUCCESS")
+             if not configValues["SUPPORTED_SSL_TLS_PROTOCOLS"]:
+                  print "No supported protocols configured"
+                  tdkTestObj.setResultStatus("SUCCESS")
+                  print "Checking TLS Fallback settings....."
+                  for line in  Result.splitlines():
+                      fallbackfound = 0;
+                      if "TLS Fallback" in line and "supports" in line:
+                          fallbackfound = 1;
+                          print "TLS Fallback supported"
+                          tdkTestObj.setResultStatus("FAILURE");
+                          break;
+                      elif "unable to determine TLS Fallback" in line:
+                          fallbackfound = 1;
+                          print "TLS Fallback not supported"
+                          tdkTestObj.setResultStatus("SUCCESS");
+                          break;
+                  if fallbackfound == 0:
+                      print "TLS Fallback Settings not found"
+                      tdkTestObj.setResultStatus("FAILURE");
+             else:
+                 supportedProtocols = configValues["SUPPORTED_SSL_TLS_PROTOCOLS"].split(",")
+                 print "Supported protocols %s"  %(supportedProtocols)
+                 print "Checking configured protocols are enabled......"
+                 for protocol in supportedProtocols:
+                      for line in Result.splitlines():
+                          if protocol in line:
+                              if "enabled" in line:
+                                  print "%s - Enabled" %(protocol)
+                                  tdkTestObj.setResultStatus("SUCCESS");
+                                  break;
+                              elif "disabled" in line:
+                                  print "%s - disabled" %(protocol)
+                                  tdkTestObj.setResultStatus("FAILURE");
+                                  break;
+                 print "Checking TLS Fallback settings....."
+                 for line in  Result.splitlines():
+                     fallbackfound = 0;
+                     if "TLS Fallback" in line and "supports" in line:
+                         fallbackfound = 1;
+                         print "TLS Fallback supported"
+                         tdkTestObj.setResultStatus("SUCCESS");
+                         break;
+                     elif "unable to determine TLS Fallback" in line:
+                         fallbackfound = 1;
+                         print "TLS Fallback not supported"
+                         tdkTestObj.setResultStatus("FAILURE");
+                         break;
+                 if fallbackfound == 0:
+                     print "TLS Fallback Settings not found"
+                     tdkTestObj.setResultStatus("FAILURE");
+         else:
+             print "SSlScan failed"
+             tdkTestObj.setResultStatus("FAILURE");
+    else:
+        print "Failed to retrieve configuration values from device config file"
+        tdkTestObj.setResultStatus("FAILURE");
+    obj.unloadModule("rdkv_security");
+
+else:
+    obj.setLoadModuleStatus("FAILURE");
+    print "Failed to load module"
+
