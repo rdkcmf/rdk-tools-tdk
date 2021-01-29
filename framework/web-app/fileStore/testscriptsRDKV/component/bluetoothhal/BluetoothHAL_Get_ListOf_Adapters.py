17fe646016d8027853f91b505ea9ddc13d8984b6
Rini Gifty <rini_gifty@comcast.com>
2021-01-29 08:19:39 +0000

    RDKTT-2672: Adding Bluetooth HAL Stub
    Reason for change: Adding the makefile changes, Stub code and scripts for Bluetooth HAL Stub
    Test Procedure: Verified via TDK Test Manager
    Risks: Low
    Signed-off-by: RiniGifty<rini.g@tataelxsi.co.in>
    
    (cherry picked from commit ebdf5c256014809c6864be4a939461c3b1658ca0)
    Change-Id: I00821a0107231dc1718f8c862a6ff8831c10f80e

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/bluetoothhal/BluetoothHAL_Get_ListOf_Adapters.py
@@ -0,0 +1,122 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2021 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>3</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>BluetoothHAL_Get_ListOf_Adapters</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>BluetoothHal_GetListOfAdapters</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>Get the list of bluetooth adapters in the device</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>1</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>CT_BLUETOOTH_HAL_01</test_case_id>
+    <test_objective>To get the list of bluetooth adapters available in the device</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>Accelerator</test_setup>
+    <pre_requisite>1. Initialize the BTRCore module using BTRCore_Init()</pre_requisite>
+    <api_or_interface_used>enBTRCoreRet BTRCore_GetListOfAdapters (tBTRCoreHandle hBTRCore, stBTRCoreListAdapters* pstListAdapters);</api_or_interface_used>
+    <input_parameters>None</input_parameters>
+    <automation_approch>1. TM loads the BluetoothHal agent via the test agent.
+2 . BluetoothHal agent will invoke the api BTRCore_GetListOfAdapters to get the list of bluetooth adapters available in the DUT.
+3. TM checks if the API call is success and checks if there is atleast one bluetooth adapter available in the DUT and return SUCCESS/FAILURE status.</automation_approch>
+    <expected_output>Checkpoint 1. Verify the API call is success
+Checkpoint 2. Verify that atleast one bluetooth adapter is available in the DUT.</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>libbluetoothhalstub.so.0</test_stub_interface>
+    <test_script>BluetoothHAL_Get_ListOf_Adapters</test_script>
+    <skipped>No</skipped>
+    <release_version>M85</release_version>
+    <remarks></remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+
+#Test component to be tested
+bluetoothhalObj = tdklib.TDKScriptingLibrary("bluetoothhal","1");
+
+#IP and Port of box, No need to change,
+#This will be replaced with corresponding DUT Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+bluetoothhalObj.configureTestCase(ip,port,'BluetoothHAL_Get_ListOf_Adapters');
+
+#Get the result of connection with test component and DUT
+result =bluetoothhalObj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %result;
+bluetoothhalObj.setLoadModuleStatus(result.upper());
+
+if "SUCCESS" in result.upper():
+    expectedresult="SUCCESS"
+    #Prmitive test case which associated to this Script
+    tdkTestObj = bluetoothhalObj.createTestStep('BluetoothHal_GetListOfAdapters');
+
+    #Execute the test case in DUT
+    tdkTestObj.executeTestCase(expectedresult);
+
+    #Get the result of execution
+    actualresult = tdkTestObj.getResult();
+    print "RESULT : BluetoothHal_GetListOfAdapters : ", actualresult
+    numOfAdapters = tdkTestObj.getResultDetails();
+    print "BluetoothHal_GetListOfAdapters : Number of adapters : ", numOfAdapters
+
+    #Set the result status of execution
+    if ((actualresult == expectedresult) && (0 < numOfAdapters)) :
+        tdkTestObj.setResultStatus("SUCCESS");
+    else:
+        tdkTestObj.setResultStatus("FAILURE");
+
+    bluetoothhalObj.unloadModule("bluetoothhal");
+else:
+    print "Failed to load bluetoothhal module\n";
+    #Set the module loading status
+    bluetoothhalObj.setLoadModuleStatus("FAILURE");
