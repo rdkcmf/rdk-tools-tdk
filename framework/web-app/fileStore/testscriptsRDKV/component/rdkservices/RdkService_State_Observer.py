96e5612d4b74e6c89a9a0498852dcd8843c9dbbd
S Vithiya Lakshmi <SVithiya_Lakshmi@comcast.com>
2021-02-09 08:36:35 +0000

    RDKTT-2667: RDKService script issue fix
    Reason for change: Fixed script issues
    Test Procedure: Check using TDK TM
    Risks: Low
    Signed-off-by: S Vithiya Lakshmi <svithiya_lakshmi@comcast.com>
    
    (cherry picked from commit 7d8a7a83da3504b68e9e9d1c3737c4ead5e8ae8f)
    Change-Id: I5fad3b5ab604c711d1522beaa63d05bbb94b8d89

--- framework/web-app/fileStore/testscriptsRDKV/component/rdkservices/RdkService_State_Observer.py
+++ framework/web-app/fileStore/testscriptsRDKV/component/rdkservices/RdkService_State_Observer.py
@@ -48,12 +48,6 @@
   <skip>false</skip>
   <!--  -->
   <box_types>
-    <box_type>RPI-Client</box_type>
-    <!--  -->
-    <box_type>RPI-HYB</box_type>
-    <!--  -->
-    <box_type>Video_Accelerator</box_type>
-    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDK2.0</rdk_version>
