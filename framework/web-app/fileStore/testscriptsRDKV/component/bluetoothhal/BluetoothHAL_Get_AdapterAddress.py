a6b8c60d717a5c35d657139413753a3e6601f60a
Rini Gifty <rini_gifty@comcast.com>
2021-02-12 09:56:46 +0000

    RDKTT-2710: BluetoothHal Testing on client devices
    Reason for change: Updating the BluetoothHAL_Get_AdapterAddress script flow for adding GetListOfAdapters as prerequisite
    Test Procedure: Verifies via TDK Test Manager
    Risks: Low
    Signed-off-by: RiniGifty<rini.g@tataelxsi.co.in>
    Change-Id: I93b7aa78fb59ed56ce7bebe7175fd141193f35db

--- framework/web-app/fileStore/testscriptsRDKV/component/bluetoothhal/BluetoothHAL_Get_AdapterAddress.py
+++ framework/web-app/fileStore/testscriptsRDKV/component/bluetoothhal/BluetoothHAL_Get_AdapterAddress.py
@@ -17,43 +17,27 @@
 # limitations under the License.
 ##########################################################################
 '''
-<?xml version='1.0' encoding='utf-8'?>
-<xml>
-  <id></id>
-  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
-  <version>2</version>
-  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+<?xml version="1.0" encoding="UTF-8"?><xml>
+  <id/>
+  <version>5</version>
   <name>BluetoothHAL_Get_AdapterAddress</name>
-  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
-  <primitive_test_id> </primitive_test_id>
-  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_id/>
   <primitive_test_name>BluetoothHal_GetAdapterAddr</primitive_test_name>
-  <!--  -->
   <primitive_test_version>1</primitive_test_version>
-  <!--  -->
   <status>FREE</status>
-  <!--  -->
   <synopsis>To get the address of bluetooth adapter</synopsis>
-  <!--  -->
-  <groups_id />
-  <!--  -->
+  <groups_id/>
   <execution_time>1</execution_time>
-  <!--  -->
   <long_duration>false</long_duration>
-  <!--  -->
   <advanced_script>false</advanced_script>
-  <!-- execution_time is the time out time for test execution -->
-  <remarks></remarks>
-  <!-- Reason for skipping the tests if marked to skip -->
+  <remarks/>
   <skip>false</skip>
-  <!--  -->
   <box_types>
+    <box_type>IPClient-Wifi</box_type>
     <box_type>Video_Accelerator</box_type>
-    <!--  -->
   </box_types>
   <rdk_versions>
     <rdk_version>RDK2.0</rdk_version>
-    <!--  -->
   </rdk_versions>
   <test_cases>
     <test_case_id>CT_BLUETOOTH_HAL_08</test_case_id>
@@ -61,22 +45,26 @@
     <test_type>Positive</test_type>
     <test_setup>Accelerator</test_setup>
     <pre_requisite>1. Initialize the BTRCore module using BTRCore_Init()</pre_requisite>
-    <api_or_interface_used>enBTRCoreRet BTRCore_GetAdapterAddr (tBTRCoreHandle hBTRCore, unsigned char aui8adapterIdx, char* apui8adapterAddr);</api_or_interface_used>
+    <api_or_interface_used>1. enBTRCoreRet BTRCore_GetListOfAdapters (tBTRCoreHandle hBTRCore, stBTRCoreListAdapters* pstListAdapters);
+2. enBTRCoreRet BTRCore_GetAdapterAddr (tBTRCoreHandle hBTRCore, unsigned char aui8adapterIdx, char* apui8adapterAddr);</api_or_interface_used>
     <input_parameters>adapter_number - using the default adapter 0</input_parameters>
     <automation_approch>1. TM loads the BluetoothHal agent via the test agent.
-2 . BluetoothHal agent will invoke the api BTRCore_GetAdapterAddr to get the address of bluetooth adapter specified using the adapter number 0(for default adapter).
-3. TM checks if the API call is success and checks if the bluetooth adapter address retrieved is not empty and return SUCCESS/FAILURE status.</automation_approch>
-    <expected_output>Checkpoint 1. Verify the API call is success
-Checkpoint 2. Verify that the bluetooth adapter address retrieved is not empty </expected_output>
+2 . BluetoothHal agent will invoke the api BTRCore_GetListOfAdapters which fetches and saves the number of bluetooth adapters available in the DUT inside HAL.
+3. BluetoothHal agent will invoke the api BTRCore_GetAdapterAddr to get the address of bluetooth adapter specified using the adapter number 0(for default adapter).
+4. TM checks if the API call is success and checks if the bluetooth adapter address retrieved is not empty and return SUCCESS/FAILURE status.</automation_approch>
+    <expected_output>Checkpoint 1. Verify the API calls are success
+Checkpoint 2. Verify that atleast one bluetooth adapter is available in the DUT.
+Checkpoint 3. Verify that the bluetooth adapter address retrieved is not empty </expected_output>
     <priority>High</priority>
     <test_stub_interface>libbluetoothhalstub.so.0</test_stub_interface>
     <test_script>BluetoothHAL_Get_AdapterAddress</test_script>
     <skipped>No</skipped>
-    <release_version>M85</release_version>
-    <remarks></remarks>
+    <release_version>M86</release_version>
+    <remarks/>
   </test_cases>
-  <script_tags />
+  <script_tags/>
 </xml>
+
 '''
 # use tdklib library,which provides a wrapper for tdk testcase script
 import tdklib;
@@ -97,10 +85,7 @@ bluetoothhalObj.setLoadModuleStatus(result.upper());
 
 if "SUCCESS" in result.upper():
     expectedresult="SUCCESS"
-    #Primitive test case which associated to this Script
-    tdkTestObj = bluetoothhalObj.createTestStep('BluetoothHal_GetAdapterAddr');
-    #Set the adapter number to '0' the default adapter number
-    tdkTestObj.addParameter("adapter_number", 0)
+    tdkTestObj = bluetoothhalObj.createTestStep('BluetoothHal_GetListOfAdapters');
 
     #Execute the test case in DUT
     tdkTestObj.executeTestCase(expectedresult);
@@ -108,17 +93,41 @@ if "SUCCESS" in result.upper():
     #Get the result of execution
     actualresult = tdkTestObj.getResult();
 
-    if (actualresult == expectedresult) :
-        print "BluetoothHal_GetAdapterAddr executed succesfully"
-        adapterAddress = tdkTestObj.getResultDetails();
-	if (adapterAddress):
-            print "Deafult adapter address : ", adapterAddress
+    #Set the result status of execution
+    if (actualresult == expectedresult):
+        print "Successfully executed BluetoothHal_GetListOfAdapters"
+        numOfAdapters = tdkTestObj.getResultDetails();
+        print "BluetoothHal_GetListOfAdapters: Number of adapters : ", numOfAdapters
+        if (0 < numOfAdapters):
             tdkTestObj.setResultStatus("SUCCESS");
+            #Primitive test case which associated to this Script
+            tdkTestObj = bluetoothhalObj.createTestStep('BluetoothHal_GetAdapterAddr');
+            #Set the adapter number to '0' the default adapter number
+            tdkTestObj.addParameter("adapter_number", 0)
+
+            #Execute the test case in DUT
+            tdkTestObj.executeTestCase(expectedresult);
+
+            #Get the result of execution
+            actualresult = tdkTestObj.getResult();
+
+            if (actualresult == expectedresult) :
+                print "BluetoothHal_GetAdapterAddr executed succesfully"
+                adapterAddress = tdkTestObj.getResultDetails();
+	        if (adapterAddress):
+                    print "Deafult adapter address : ", adapterAddress
+                    tdkTestObj.setResultStatus("SUCCESS");
+                else:
+                    print "Adapter address should not be empty"
+                    tdkTestObj.setResultStatus("FAILURE");
+            else:
+                print "BluetoothHal_GetAdapterAddr : failed"
+                tdkTestObj.setResultStatus("FAILURE");
         else:
-            print "Adapter address should not be empty"
+            print "Atleast default adapter should be present in the device"
             tdkTestObj.setResultStatus("FAILURE");
     else:
-        print "BluetoothHal_GetAdapterAddr : failed"
+        print "Failed to execute BluetoothHal_GetListOfAdapters"
         tdkTestObj.setResultStatus("FAILURE");
 	        
     #Unload the module
