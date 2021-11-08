a8d47135181316b4e52f339638706fe30642ad52
spugaz546 <Srigayathry_Pugazhenthi@comcast.com>
2021-11-08 06:33:34 +0000

    RDKTT-3351: MVS Coverage Enhancements
    Reason for change: 1.Updated MSE/EME test version number
    
    Test Procedure: Check if test success
    Risks: None
    Signed-off-by: Srigayathry P <Srigayathry_Pugazhenthi@comcast.com>
    Change-Id: I3f6b8d6df0ed254a2d6cf2e6c2b8cb21a5549f12

--- framework/web-app/fileStore/MediaValidationVariables.py
+++ framework/web-app/fileStore/MediaValidationVariables.py
@@ -328,8 +328,8 @@ image_upload_dir = ""
 #            CONFIGURATIONS FOR MSE/EME TESTS
 #************************************************************************
 #User shall update the URL as per the version required
-mse_conformance_test_app_url = "https://ytlr-cert.appspot.com/2020/main.html?test_type=conformance-test"
-eme_conformance_test_app_url = "https://ytlr-cert.appspot.com/2020/main.html?test_type=encryptedmedia-test"
+mse_conformance_test_app_url = "https://ytlr-cert.appspot.com/2021/main.html?test_type=conformance-test"
+eme_conformance_test_app_url = "https://ytlr-cert.appspot.com/2021/main.html?test_type=encryptedmedia-test"
 
 #Key codes to navigate and start the test. Values must follow the pattern KeyName:KeyCode seperated by comma
 #Key sequence should be updated corresponding to the test URL used by the user
