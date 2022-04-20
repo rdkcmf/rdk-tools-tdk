6cb3c7ae4c682067ddb19d8b5385a22f087ff217
Riya Alias <riya_alias@comcast.com>
2022-04-20 15:20:24 +0000

    RDKTT-3719: Adding new dshal script
    Reason for change: Adding new TDK dshal script for enabling Bass Enhnacer
    Test Procedure: Verify with TDK TM
    Risks: None
    Signed-off-by: Riya Alias <riya_alias@comcast.com>
    Change-Id: I6f6a9ff1b4cb7867b7f3385b3ab19f921d0a312a

---
+++ framework/web-app/fileStore/testscriptsRDKV/component/dshal/DSHal_SetBassEnhancer_Invalid.py
@@ -0,0 +1,146 @@
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2022 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+'''
+<?xml version='1.0' encoding='utf-8'?>
+<xml>
+  <id></id>
+  <!-- Do not edit id. This will be auto filled while exporting. If you are adding a new script keep the id empty -->
+  <version>3</version>
+  <!-- Do not edit version. This will be auto incremented while updating. If you are adding a new script you can keep the vresion as 1 -->
+  <name>DSHal_SetBassEnhancer_Invalid</name>
+  <!-- If you are adding a new script you can specify the script name. Script Name should be unique same as this file name with out .py extension -->
+  <primitive_test_id></primitive_test_id>
+  <!-- Do not change primitive_test_id if you are editing an existing script. -->
+  <primitive_test_name>DSHal_GetBassEnhancer</primitive_test_name>
+  <!--  -->
+  <primitive_test_version>1</primitive_test_version>
+  <!--  -->
+  <status>FREE</status>
+  <!--  -->
+  <synopsis>To check if Bass Enhancer is set to invalid value</synopsis>
+  <!--  -->
+  <groups_id />
+  <!--  -->
+  <execution_time>2</execution_time>
+  <!--  -->
+  <long_duration>false</long_duration>
+  <!--  -->
+  <advanced_script>false</advanced_script>
+  <!-- execution_time is the time out time for test execution -->
+  <remarks></remarks>
+  <!-- Reason for skipping the tests if marked to skip -->
+  <skip>false</skip>
+  <!--  -->
+  <box_types>
+    <box_type>IPClient-Wifi</box_type>
+    <box_type>Video_Accelerator</box_type>
+    <!--  -->
+  </box_types>
+  <rdk_versions>
+    <rdk_version>RDK2.0</rdk_version>
+    <!--  -->
+  </rdk_versions>
+  <test_cases>
+    <test_case_id>CT_DS_HAL_167</test_case_id>
+    <test_objective>To check if Bass Enhancer is set to invalid value</test_objective>
+    <test_type>Positive</test_type>
+    <test_setup>XI6,Video_Accelerator</test_setup>
+    <pre_requisite>1. Initialize IARMBus
+2. Connect IARMBus
+3. Initialize dsMgr
+4. Initialize DSHAL subsystems</pre_requisite>
+    <api_or_interface_used>dsGetAudioPort(dsAudioPortType_t type, int index, int *handle)
+dsSetBassEnhancer(int handle, int boost)
+dsGetBassEnhancer(int handle, int *boost)</api_or_interface_used>
+    <input_parameters>type - Audio port type
+index- Audio port index
+boost - boostvalue</input_parameters>
+    <automation_approch>1. TM loads the DSHAL agent via the test agent.
+2 . DSHAL agent will invoke the api dsSetBassEnhancer to set invalid boost value
+3 . TM checks if the boost value is same as that set and return SUCCESS/FAILURE status.</automation_approch>
+    <expected_output>Checkpoint 1.Verify the API call is failure</expected_output>
+    <priority>High</priority>
+    <test_stub_interface>libdshalstub.so.0.0.0</test_stub_interface>
+    <test_script>DSHal_SetBassEnhancer_Invalid</test_script>
+    <skipped>No</skipped>
+    <release_version>M100</release_version>
+    <remarks></remarks>
+  </test_cases>
+  <script_tags />
+</xml>
+'''
+# use tdklib library,which provides a wrapper for tdk testcase script
+import tdklib;
+import deviceCapabilities;
+from dshalUtility import *;
+#Test component to be tested
+dshalObj = tdklib.TDKScriptingLibrary("dshal","1");
+#IP and Port of box, No need to change,
+#This will be replaced with correspoing Box Ip and port while executing script
+ip = <ipaddress>
+port = <port>
+dshalObj.configureTestCase(ip,port,'DSHal_SetBassEnhancer_Invalid');
+#Get the result of connection with test component and STB
+dshalloadModuleStatus = dshalObj.getLoadModuleResult();
+print "[LIB LOAD STATUS]  :  %s" %dshalloadModuleStatus;
+#Check if BassEnhancer is supported by the DUT
+capable = deviceCapabilities.getconfig(dshalObj,"BassEnhancer");
+if "SUCCESS" in dshalloadModuleStatus.upper() and capable:
+    expectedResult="SUCCESS";
+    dshalObj.setLoadModuleStatus(dshalloadModuleStatus);
+    #Prmitive test case which associated to this Script
+    tdkTestObj = dshalObj.createTestStep('DSHal_GetAudioPort');
+    tdkTestObj.addParameter("portType", audioPortType["HDMI"]);
+    #Execute the test case in STB
+    tdkTestObj.executeTestCase(expectedResult);
+    actualResult = tdkTestObj.getResult();
+    print "DSHal_GetAudioPort result: ", actualResult
+    if expectedResult in actualResult:
+        tdkTestObj.setResultStatus("SUCCESS");
+        details = tdkTestObj.getResultDetails();
+        print details;
+        boost = 120;
+        print "Trying to set Bass Enhancer to", boost;
+        #Prmitive test case which associated to this Script
+        tdkTestObj = dshalObj.createTestStep('DSHal_SetBassEnhancer');
+        tdkTestObj.addParameter("boost", boost);
+        #Execute the test case in STB
+        tdkTestObj.executeTestCase(expectedResult);
+        actualResult = tdkTestObj.getResult();
+        print "DSHal_SetBassEnhancer result: ", actualResult
+        expectedResult="FAILURE";
+        if expectedResult in actualResult:
+            tdkTestObj.setResultStatus("SUCCESS");
+            details = tdkTestObj.getResultDetails();
+            print "DSHal_SetBassEnhancer failed for invalid boost";
+        else:
+            tdkTestObj.setResultStatus("FAILURE");
+            print "DSHal_DialogEnhancement success for invalid boost";
+
+    else:
+        tdkTestObj.setResultStatus("FAILURE");
+        print "AudioPort handle not retrieved";
+    dshalObj.unloadModule("dshal");
+elif not capable and "SUCCESS" in dshalloadModuleStatus.upper():
+    print "Exiting from script";
+    dshalObj.setLoadModuleStatus("FAILURE");
+    dshalObj.unloadModule("dshal");
+else:
+    print "Module load failed";
+
